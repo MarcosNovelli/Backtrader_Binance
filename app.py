9794456270bdfc34ac76ddf9720b25be3121693b
Marcos Novelli <marcossnovelli@gmail.com>
2022-09-27 17:19:33 -0300

    added DBD, tp and stop

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from functions import store_ticker_prices_backtest
 import backtrader as bt
 from teststrategies import SmaCross, SupplyAndDemand, TestStrategy
 
-df = store_ticker_prices_backtest("BTCUSDT", "1h", 10)
+df = store_ticker_prices_backtest("BTCUSDT", "1h")  
 print(df)
 cerebro = bt.Cerebro()
 
@@ -12,7 +12,6 @@ feed = bt.feeds.PandasData(dataname=df)
 cerebro.adddata(feed)
 
 cerebro.addstrategy(SupplyAndDemand)
-cerebro.broker.setcommission(commission=0.005)
 cerebro.addsizer(bt.sizers.PercentSizer, percents=50)
 
 cerebro.run()
