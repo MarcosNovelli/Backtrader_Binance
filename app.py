da76a3ef2f1eb1ddfd62d436881f75d4cfe639f2
Marcos Novelli <marcossnovelli@gmail.com>
2022-10-06 12:25:44 -0300

    probando con roma

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from config import client
-from functions import store_ticker_prices_backtest
+from functions import store_ticker_prices_backtest, get_data_since
 import backtrader as bt
 from teststrategies import SmaCross, SupplyAndDemand, TestStrategy
+import datetime as dt
 
-df = store_ticker_prices_backtest("BTCUSDT", "1m")  
+
+startTime = dt.datetime(2022, 10, 6)
+endTime = dt.datetime.now(dt.timezone.utc)
+
+
+df = get_data_since("BTCUSDT", "1m", startTime, endTime)
+
+# print(store_ticker_prices_backtest("BTCUSDT", "1m", startTime, endTime))
+
+
 print(df)
 cerebro = bt.Cerebro()
 
@@ -12,9 +22,5 @@ cerebro.adddata(feed)
 
 cerebro.addstrategy(SupplyAndDemand)
 cerebro.addsizer(bt.sizers.PercentSizer, percents=50)
-
 cerebro.run()
-cerebro.plot(style="candlestick")
-
-#puedo agarrar slices con el get, pero no se que me devuelve, ver bien si puedo agarrar low los con data.low
-# cambiar la cantidad a 10 barras
+cerebro.plot(style="candlestick", fmt_x_data = '%Y-%b-%d %H:%M')
--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+from tracemalloc import start
 from config import client
 from binance.enums import *
 from typing import List
@@ -9,11 +10,17 @@ import pandas as pd
 '''
 
 
-def store_ticker_prices_backtest(symbol:str, TF:str, lookback:int=1000) -> pd.DataFrame:
-    
+def store_ticker_prices_backtest(symbol:str, TF:str, startTime, endTime, lookback:int=1000) -> pd.DataFrame:
+
+    startTime = str(int(startTime.timestamp() * 1000))
+    endTime = str(int((endTime + dt.timedelta(0, 10800) ).timestamp()* 1000))
+
+
     # Devuelve hasta el current candle, con los parametros que tenga en el momento
-    klines:list = client.get_historical_klines(symbol, TF, limit=lookback, klines_type=HistoricalKlinesType.FUTURES)
+    klines:list = client.get_historical_klines(symbol, TF, limit=lookback, start_str=startTime, end_str=endTime, klines_type=HistoricalKlinesType.FUTURES)
     
+    
+
     # Create dataframe with all candlestick info
     df = pd.DataFrame(klines)
     
@@ -35,4 +42,24 @@ def store_ticker_prices_backtest(symbol:str, TF:str, lookback:int=1000) -> pd.Da
     
     return df
 
+
+def get_data_since(ticker, TF, startTime, endTime):        
+        df_list = []
+        
+        while True:                
+                print(startTime, endTime)
+
+                new_df = store_ticker_prices_backtest(ticker, TF, startTime, endTime )
+                if new_df is None:
+                        break
+                
+                if startTime >= endTime:
+                        break
+
+                df_list.append(new_df)
+                startTime = max(new_df.index) + dt.timedelta(0, 60)
+                
+        all_price_df = pd.concat(df_list)
+        all_price_df.to_csv('price_data.csv')
+        return all_price_df
     
