93bacf70e0fcb2f800d293b0eb90f28e8e987ffd
Marcos Novelli <marcossnovelli@gmail.com>
2022-10-08 15:19:23 -0300

    Can now get data from past and re use it

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 from config import client
-from functions import store_ticker_prices_backtest, get_data_since
+from functions import store_ticker_prices_backtest, get_data_since, get_file_data
 import backtrader as bt
 from teststrategies import SmaCross, SupplyAndDemand, TestStrategy
 import datetime as dt
+import pandas as pd
+
+# Choose to download(1) or already downloaded data(0)
+download = 0
 
 # Both in seconds
-startTime = (dt.datetime(2022, 9, 6)).timestamp()
+startTime = (dt.datetime(2022, 10, 6)).timestamp()
 endTime = dt.datetime.now().timestamp()
 
-df = get_data_since("BTCUSDT", "1m", startTime, endTime)
-
-# df = store_ticker_prices_backtest("BTCUSDT", "1m", 1665092760000, endTime)
-
-
+df = get_data_since("BTCUSDT", "1m", startTime, endTime) if download else get_file_data('price_data.csv')
 
 print(df)
-# cerebro = bt.Cerebro()
+cerebro = bt.Cerebro()
 
-# feed = bt.feeds.PandasData(dataname=df)
-# cerebro.adddata(feed)
+feed = bt.feeds.PandasData(dataname=df)
+cerebro.adddata(feed)
 
-# cerebro.addstrategy(SupplyAndDemand)
-# cerebro.addsizer(bt.sizers.PercentSizer, percents=50)
-# cerebro.run()
-# cerebro.plot(style="candlestick", fmt_x_data = '%Y-%b-%d %H:%M')
+cerebro.addstrategy(SupplyAndDemand)
+cerebro.addsizer(bt.sizers.PercentSizer, percents=50)
+cerebro.run()
+cerebro.plot(style="candlestick", fmt_x_data = '%Y-%b-%d %H:%M')
--- functions.py
+++ functions.py
@@ -4,11 +4,12 @@ from binance.enums import *
 from typing import List
 import datetime as dt
 import pandas as pd
+import itertools
+import backtrader as bt
 
 '''
         :return: list of OHLCV values (Open time, Open, High, Low, Close, Volume, Close time, Quote asset volume, Number of trades, Taker buy base asset volume, Taker buy quote asset volume, Ignore)
 '''
-
 
 def store_ticker_prices_backtest(symbol:str, TF:str, startTime, endTime=dt.datetime.now().timestamp(), lookback:int=1000) -> pd.DataFrame:
 
@@ -56,3 +57,20 @@ def get_data_since(ticker, TF, startTime, endTime):
         
         return all_price_df
     
+def get_file_data(file:str):
+        df = pd.read_csv(file, delimiter=',')
+        
+        df = df.iloc[:,1:]
+
+        df.columns = ["Date", "Open", "High", "Low", "Close", "Volume", "Adj_Close"]
+
+        df.Open      = df.Open.astype("float")
+        df.High      = df.High.astype("float")
+        df.Low       = df.Low.astype("float")
+        df.Close     = df.Close.astype("float")
+        df.Volume    = df.Volume.astype("float")
+        df.Adj_Cose  = df.Adj_Close.astype("float")
+
+        df.index = [dt.datetime.fromtimestamp(x/1000.0) for x in df.Date]
+
+        return df
