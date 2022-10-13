b2227a64383d6b234374566f200964e529392964
Marcos Novelli <marcossnovelli@gmail.com>
2022-10-13 15:31:08 -0300

    bot is working and detecting hourly DBD

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from config import client
-from functions import store_ticker_prices_backtest, get_data_since, get_file_data
+from functions import get_data_since, get_file_data
 import backtrader as bt
 from teststrategies import SmaCross, SupplyAndDemand, TestStrategy
 import datetime as dt
 import pandas as pd
 
 # Choose to download(1) or already downloaded data(0)
-download = 0
+download = 1
 
 # Both in seconds
-startTime = (dt.datetime(2022, 10, 6)).timestamp()
+startTime = (dt.datetime(2021, 10, 13)).timestamp()
+# endTime = (dt.datetime(2022, 10, 9)).timestamp()
 endTime = dt.datetime.now().timestamp()
 
 df = get_data_since("BTCUSDT", "1m", startTime, endTime) if download else get_file_data('price_data.csv')
@@ -21,6 +22,7 @@ feed = bt.feeds.PandasData(dataname=df)
 cerebro.adddata(feed)
 
 cerebro.addstrategy(SupplyAndDemand)
-cerebro.addsizer(bt.sizers.PercentSizer, percents=50)
+cerebro.broker.setcash(1000)
+cerebro.addsizer(bt.sizers.PercentSizer, percents=10)
 cerebro.run()
 cerebro.plot(style="candlestick", fmt_x_data = '%Y-%b-%d %H:%M')
